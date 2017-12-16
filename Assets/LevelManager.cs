562232a9a4b1064c888d73d9c5f172ba41b50b42
h2jie <h2jie09@gmail.com>
2017-12-16 15:47:47 +0100

    Death effects

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class LevelManager : MonoBehaviour {
     public PlayerController playerController;
     public float waitSecont;
+    public GameObject deadExplosion;
 	// Use this for initialization
 	void Start () {
         playerController = FindObjectOfType<PlayerController>();	
@@ -22,6 +23,7 @@ public class LevelManager : MonoBehaviour {
     public IEnumerator RespawnCo()
     {
         playerController.gameObject.SetActive(false);
+        Instantiate(deadExplosion,playerController.transform.position,playerController.transform.rotation);
         yield return new WaitForSeconds(waitSecont);
         playerController.transform.position = playerController.RespawnPosition;
         playerController.gameObject.SetActive(true);
