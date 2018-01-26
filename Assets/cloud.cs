b37f653bedb3be82f06123e0875e5f47108e3a70
h2jie <h2jie09@gmail.com>
2018-01-26 17:44:16 +0100

    object cloud with stab created

---
+++ Assets/cloud.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class cloud : MonoBehaviour {
+
+    public UnityEvent onCollision;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        onCollision.Invoke();
+    }
+}
